63dc53feafd3ec7c76f291380c94413c7352b4c4
vivianyee5534 <vivian.yee@stonybrook.edu>
2024-02-29 14:06:59 -0500

    loading icon

--- pages/guestlist.tsx
+++ pages/guestlist.tsx
@@ -1,15 +1,17 @@
-import clientPromise from "@lib/mongo";
 import Layout from "@shared/components/Layout";
 import Head from "next/head";
 import { useContext, useEffect, useState } from "react";
 import { GuestContext } from "./_app";
 import Subheading from "@shared/components/Subheading";
+import { LoadingOutlined } from "@ant-design/icons";
 
-export default function GuestList({ allGuests }) {
+export default function GuestList() {
   const guestContext = useContext(GuestContext);
-  const [guestList, setGuestList] = useState(allGuests);
+  const [guestList, setGuestList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch("http://localhost:3000/api/guestlist", {
       method: "GET",
       headers: {
@@ -19,6 +21,11 @@ export default function GuestList({ allGuests }) {
       .then((res) => res.json())
       .then((data) => {
         setGuestList(data.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        console.log(`Error fetching guest list ${e}`);
       });
   }, [guestContext.submitForm]);
 
@@ -38,24 +45,29 @@ export default function GuestList({ allGuests }) {
           <h4 className="text-sm md:text-lg pb-1 md:pb-3">
             Post by clicking the ribbon above and sending a message!
           </h4>
-          <div className="overflow-y-scroll description">
-            {[...guestList].reverse().map((guest, i) => {
-              return (
-                <div
-                  className="border-white border-t-2 flex justify-between p-2"
-                  key={guest.name + i}
-                >
-                  <div className="pr-3">
-                    <h4 className="text-sm md:text-base">{guest.name}</h4>
-                    <h4 className="text-xs md:text-s">
-                      {new Date(guest.date).toLocaleDateString("en-US")}
-                    </h4>
+          {loading ? (
+            <div className="text-4xl md:text-5xl py-5 text-center"><LoadingOutlined/></div>
+          ) : (
+            <div className="overflow-y-scroll description">
+              {[...guestList].reverse().map((guest, i) => {
+                const guestDate = new Date(guest.date);
+                return (
+                  <div
+                    className="border-white border-t-2 flex justify-between p-2"
+                    key={guest.name + i}
+                  >
+                    <div className="pr-3">
+                      <h4 className="text-sm md:text-base">{guest.name}</h4>
+                      <h4 className="text-xs md:text-s">
+                        {guestDate.toLocaleDateString("en-US")} {}
+                      </h4>
+                    </div>
+                    <h4 className="text-sm md:text-base">{guest.text}</h4>
                   </div>
-                  <h4 className="text-sm md:text-base">{guest.text}</h4>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
+          )}
           <h4 className="text-sm md:text-lg py-3 md:pb-3 text-center">
             You&apos;ve recently posted {guestContext.submitForm} message
             {guestContext.submitForm !== 1 && "s"}!
@@ -64,11 +76,4 @@ export default function GuestList({ allGuests }) {
       </Layout>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const client = await clientPromise;
-  const db = client.db();
-  const allGuests = await db.collection("guestlist").find({}).toArray();
-  return { props: { allGuests: JSON.parse(JSON.stringify(allGuests)) } };
 }
